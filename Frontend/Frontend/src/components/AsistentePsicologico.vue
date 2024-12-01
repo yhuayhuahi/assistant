9533da05c84f93ebca7ebf6f0f5e4542f8909e5b
JoaoEstar <jbasurco@unsa.edu.pe>
2024-12-01 11:00:12 -0500

    Modificaciones en el Frotend

---
+++ Frontend/Frontend/src/components/AsistentePsicologico.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Asistente de Ayuda Psicológica</h1>
+    <p>Bienvenido al asistente psicológico. ¿En qué puedo ayudarte hoy?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AsistentePsicologico',
+};
+</script>
+
+<style scoped>
+/* Estilos opcionales para este componente */
+</style>
