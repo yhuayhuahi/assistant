1b292696b1afd0f69e42085a8972e0f259a17f09
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-12-03 11:21:08 -0500

    Por culpa de jovo y su carpeta node_modules tmr

--- Frontend/Frontend/node_modules/vitest/dist/cli.js
+++
@@ -1,8 +0,0 @@
-import { c as createCLI } from './chunks/cac.DWAW3Uh5.js';
-import '@vitest/utils';
-import 'events';
-import 'pathe';
-import 'tinyrainbow';
-import './chunks/constants.fzPh7AOq.js';
-
-createCLI().parse();
